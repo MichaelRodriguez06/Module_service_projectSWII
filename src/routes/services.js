e018fb0cbe9c0f416a28a62608a7f0bda081a75a
ScSixto <58305940+ScSixto@users.noreply.github.com>
2022-08-05 11:14:56 -0500

    Añadido Buscar servicio

--- src/routes/services.js
+++ src/routes/services.js
@@ -101,6 +101,32 @@ router.get('/getInscription/', async (req, res) => {
 
 
 /***
+ * Get Service
+ */
+ router.get('/buscar_servicio/', async (req, res) => {
+    const services = await ServiceSchema.find();
+    let service = null;
+    let id_service = req.body.id_service;
+    services.forEach(function (element) {
+        if (id_service == element.id_service) {
+            service = element.toString();
+            res.status(200).json({
+                code: 200,
+                message: 'Servicio de limpieza encontrado',
+                details: 'Servicio: ' + element.toString()
+            });
+        }
+    });
+    if (service == null)
+        res.status(404).json({
+            code: 404,
+            message: 'No se encuentra el servicio de limpieza',
+            details: 'El servicio de limpieza ' + id_service + '. No se encuentra en la base de datos'
+        });
+
+});
+
+/***
  *  Obtener datos de una materia apartir de un ID de la materia
  */
 router.get('/getSubject/', async (req, res) => {
